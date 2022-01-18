938f71b0aceadedb16aac97c6311c38222e37ecd
Matt Broerman <matt.broerman@gmail.com>
2022-01-17 23:29:46 -0500

    implement basic validation

--- R/mod_panel_upload.R
+++ R/mod_panel_upload.R
@@ -12,17 +12,25 @@ mod_panel_upload_ui <- function(id){
   tagList(
     sidebarLayout(
       sidebarPanel(
-        fileInput(
-          inputId = ns("filedata"),
-          label = "Upload data. Choose csv file",
-          accept = c(".csv")
-        ),
-        verbatimTextOutput(ns("warning")),
-        textInput(ns("access_code"), "Access Code"),
-        actionButton(
-          ns("geolocate"),
-          label = "Geolocate"
-        )
+        fileInput(ns("filedata"), "Choose your file in csv",
+                  multiple = FALSE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")),
+        actionButton(ns("v_file"),"Validate File"),
+        uiOutput(ns("valid_message")),
+        hr()
+        # verbatimTextOutput("view_valid_data"),
+        # fileInput(
+        #   inputId = ns("filedata"),
+        #   label = "Upload data. Choose csv file",
+        #   accept = c(".csv")
+        # ),
+        # textInput(ns("access_code"), "Access Code"),
+        # actionButton(
+        #   ns("geolocate"),
+        #   label = "Geolocate"
+        # )
       ),
       mainPanel(
         verbatimTextOutput(ns("view_data")),
@@ -51,15 +59,54 @@ mod_panel_upload_server <- function(id){
     
     output$view_data <- renderPrint(head(dataset()))
     
+    observeEvent(input$v_file,{
+      if(is.null(input$filedata$datapath)){
+        output$valid_message <- renderUI(
+          tags$div(
+            tags$br(),
+            "Please upload a csv file.", 
+            tags$br()
+          )
+        ) 
+      } else {
+        # dataset <- input$filedata$datapath %>% 
+        #   read.csv()
+        
+        if (!is.element("address", colnames(dataset()))) {
+          output$valid_message <- renderUI(
+            tags$div(
+              tags$br(),
+              "'address' must be a column name.", 
+              tags$br()
+            )
+          )
+        } else if (length(unique(dataset()$address)) != length(dataset()$address)) {
+          output$valid_message <- renderUI(
+            tags$div(
+              tags$br(),
+              "Addresses must be unique.", 
+              tags$br()
+            )
+          )
+        } else {
+          output$valid_message <- renderUI(
+            tags$div(
+              tags$br(),
+              textInput(session$ns("access_code"), "Access Code"),
+              actionButton(session$ns("geo_file"),"Geolocate File")
+            )
+          )
+        }
+      }
+    })
+    
     # x = [-79.95, -80.00], y = [40.35, 40.50]
-    dataset_geo <- eventReactive(input$geolocate, {
+    dataset_geo <- eventReactive(input$geo_file, {
       if (input$access_code == "geo"){
         dataset() %>% 
           mutate(x = runif(n(), min = -80.00, max = -79.95)) %>% 
           mutate(y = runif(n(), min = 40.35, max = 40.50)) %>% 
           return()
-      } else {
-        return(NULL)
       }
     })
     
